ddf4fe2a2c63d62543d6040fd281a0ca598e7fef
[Alex] <alex.the.dev404@gmail.com>
2016-05-09 00:15:45 +1200

    Added comments to config

--- config.py
+++ config.py
@@ -5,11 +5,17 @@ url_list ="urls.csv"
 email_list = 1
 
 #status codes you wish to watch for and email.
-status_codes = [404, 301]
+status_codes = [404]
 
-user_name = "alexs.test.mail@gmail.com"
-password = "blackbean123"
-email_address = "alexs.test.mail@gmail.com"
-smtp_server = "smtp.gmail.com"
-smtp_port = "587"
-email_dest_address = "drmcpirateninja@gmail.com"
+#Your email username. This is the username of the email account you are sending the message from
+user_name = ""
+#Your email password
+password = ""
+#Your email address
+email_address = ""
+#The email address smtp e.g. smtp.google.com
+smtp_server = ""
+#The email address smtp port e.g. 25, 587
+smtp_port = ""
+#the destonation email address for the URL report
+email_dest_address = ""
